53675b531f22b8de28e9e986a4a521e95624e0e0
Shubham Singh Khati <shubhamkhati2001@gmail.com>
2023-06-02 00:43:49 +0530

    name removed

--- src/Header.jsx
+++ src/Header.jsx
@@ -24,7 +24,7 @@ function Header() {
         <div className='header__nav'>
             <Link to="/login" className='header__link'>
               <div className='header__option'>
-                <span className='header__optionLineOne'>Hello, Shubham</span>
+                <span className='header__optionLineOne'>Hello,</span>
                 <span className='header__optionLineTwo'>Sign In</span>
               </div>
             </Link>
