043548750c94fe30ac65585b3d01217800e5cce4
Shubham Singh Khati <shubhamkhati2001@gmail.com>
2023-06-02 15:09:04 +0530

    price changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,15 +10,15 @@ function Home() {
       <Product 
        id="23234123"
        title="This is one of the product among all other present on amazon" 
-       price={11.98}
+       price={1.98}
        rating={3}
        image="https://www.helium10.com/app/uploads/2020/04/vit-c.jpg"
        />
        <Product 
        id="23234123"
        title="This is one of the product among all other present on amazon"
-       price={11.98}
-       rating={3}
+       price={100.98}
+       rating={5}
        image="https://img.buzzfeed.com/buzzfeed-static/static/2016-08/18/19/asset/buzzfeed-prod-fastlane02/sub-buzz-21973-1471563569-1.jpg"
        />
       </div>
@@ -27,22 +27,22 @@ function Home() {
       <Product 
        id="23234123"
        title="This is one of the product among all other present on amazon"
-       price={11.98}
-       rating={3}
+       price={11.99}
+       rating={2}
        image="https://www.junglescout.com/wp-content/uploads/2021/01/product-photo-water-bottle-hero.png"
        />
        <Product 
        id="23234123"
        title="This is one of the product among all other present on amazon"
-       price={11.98}
-       rating={3}
+       price={9}
+       rating={4}
        image="https://d3rlgjrg24xpvv.cloudfront.net/wp-content/uploads/2017/12/26073945/Amazon-A-Product-Image-Service-1.jpg"
        />
        <Product 
        id="23234123"
        title="This is one of the product among all other present on amazon"
-       price={11.98}
-       rating={3}
+       price={84}
+       rating={4}
        image="https://www.junglescout.com/wp-content/uploads/2021/01/product-photo-limits.png"
        />
       </div>
@@ -50,8 +50,8 @@ function Home() {
       <Product 
        id="23234123"
        title="This is one of the product among all other present on amazon"
-       price={11.98}
-       rating={3}
+       price={111.49}
+       rating={5}
        image="https://www.junglescout.com/wp-content/uploads/2021/01/product-photo-quiz.png"
        />
       </div>
